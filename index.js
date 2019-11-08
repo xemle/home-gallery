a57d030e5131764c45ab1826a05b53bcf0071ddf
Sebastian Felis <sebastian@silef.de>
2019-11-08 14:19:16 +0100

    Make checksum optional with flag -c. Default is off

--- index.js
+++ index.js
@@ -8,22 +8,24 @@ const sha1sum = require('./lib/sha1');
 const args = process.argv.slice(2);
 
 const options = {
-  base: path.resolve(args[0] || '.'),
-  indexFilename: 'fs.index'
+  base: '.',
+  indexFilename: 'fs.index',
+  checksum: false
 }
 
 while (args.length) {
   const arg = args.shift();
-  if (arg === '-d') {
+  if (arg === '-d' || arg === '--directory') {
     const param = args.shift();
     options.base = path.resolve(param)
-  }
-  if (arg === '-i') {
+  } else if (arg === '-i' || arg === '--index') {
     const param = args.shift();
     options.indexFilename = path.resolve(param);
-  }
-  if (arg === '-h' || arg === '--help') {
-    console.log(`-d <directory> -i <index file>`);
+  } else if (arg === '-c' || arg === '--checksum') {
+    debug(`Enable checksum calculation`);
+    options.checksum = true;
+  } else if (arg === '-h' || arg === '--help') {
+    console.log(`-d|--directory <directory> -i|--index <index file> [-c|--checksum]`);
     process.exit(0);
   }
 }
@@ -31,7 +33,13 @@ while (args.length) {
 const t0 = Date.now();
 async.waterfall([
   (callback) => fileIndex(options.base, options.indexFilename, callback),
-  (index, callback) => sha1sum(options.indexFilename, index, callback)
+  (index, callback) => {
+    if (options.checksum) {
+      return sha1sum(options.indexFilename, index, callback)
+    } else {
+      return callback(null, index);
+    }
+  }
 ], (err) => {
   if (!err) {
     debug(`Successfully updated file index in ${Date.now() - t0}ms`);
