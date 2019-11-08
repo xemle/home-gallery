b03e6c8faade9bba4ab0c3a72e04b31b4abd0d08
Sebastian Felis <sebastian@silef.de>
2019-11-08 12:36:23 +0100

    Modularize sha1 calculation

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-const fs = require('fs');
 const path = require('path');
-const crypto = require('crypto');
 const async = require('async');
 const debug = require('debug')('core');
 
 const fileIndex = require('./lib/file-index');
-const writeIndex = require('./lib/index/write');
+const sha1sum = require('./lib/sha1');
 
 const args = process.argv.slice(2);
 
@@ -33,51 +31,7 @@ while (args.length) {
 const t0 = Date.now();
 async.waterfall([
   (callback) => fileIndex(options.base, options.indexFilename, callback),
-  (index, callback) => {
-    const missingSha1Entries = index.entries.filter(e => e.isFile && !e.sha1sum);
-    let interrupted = false;
-    const t0 = Date.now();
-
-    if (!missingSha1Entries.length) {
-      return callback(null, index);
-    }
-
-    debug(`Calculating SHA1 from ${missingSha1Entries.length} entries`);
-
-    process.on('SIGINT', () => {
-      console.log('Graceful shutdown...');
-      interrupted = true;
-      writeIndex(options.indexFilename, index, callback);
-    });
-
-    const calculate = (entries, done) => {
-      if (!entries.length || interrupted) {
-        return done();
-      }
-      const entry = entries.shift();
-      const filename = path.join(options.base, entry.filename);
-
-      var input = fs.createReadStream(filename);
-      var digest = crypto.createHash('sha1');
-
-      input.addListener('error', done);
-      input.addListener('data', (data) => digest.update(data));
-      input.addListener('close', () => {
-        entry.sha1sum = digest.digest('hex');
-        debug(`Calculated SHA1 of ${entry.filename}`);
-        calculate(entries, done);
-      })
-    }
-
-    calculate(missingSha1Entries, (err) => {
-      if (err) {
-        return callback(err);
-      }
-      debug(`All SHA1 are calculated in ${Date.now() - t0}ms`);
-      writeIndex(options.indexFilename, index, callback);
-    });
-
-  }
+  (index, callback) => sha1sum(options.indexFilename, index, callback)
 ], (err) => {
   if (!err) {
     debug(`Successfully updated file index in ${Date.now() - t0}ms`);
