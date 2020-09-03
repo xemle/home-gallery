3a1c863873587ab9f493ceadb7e40e84b3fa00bb
Sebastian Felis <sebastian@silef.de>
2020-09-03 22:07:49 +0200

    Extract events to separate package

--- packages/webapp/src/store/event-model.ts
+++ packages/webapp/src/store/event-model.ts
@@ -2,18 +2,7 @@ import { Thunk, thunk } from 'easy-peasy';
 import { Entry } from './entry-model';
 import { StoreModel } from './store';
 
-export interface Event {
-  type: string;
-  id: string;
-  date: string;
-  targetIds: string[];
-  actions: EventAction[];
-}
-
-export interface EventAction {
-  action: string;
-  value: any;
-}
+import { applyEvents, Event } from '@home-gallery/events';
 
 export interface EventModel {
   events: Event[];
@@ -21,66 +10,6 @@ export interface EventModel {
   initEvents: Thunk<EventModel, Event[]>;
   addEvent: Thunk<EventModel, Event>;
   applyEvents: Thunk<EventModel, Event[], any, StoreModel>;
-}
-
-const applyEventAction = (entry: Entry, action: EventAction) => {
-  let resetTextCache = false;
-  switch (action.action) {
-    case 'addTag': {
-      if (!entry.tags) {
-        entry.tags = [];
-      }
-      if (entry.tags.indexOf(action.value) < 0) {
-        entry.tags.push(action.value);
-        resetTextCache = true;
-      }
-      break;
-    }
-    case 'removeTag': {
-      if (!entry.tags || !entry.tags.length) {
-        return
-      }
-      const index = entry.tags.indexOf(action.value);
-      if (index >= 0) {
-        entry.tags.splice(index, 1);
-        resetTextCache = true;
-      }
-      break;
-    }
-  }
-  if (resetTextCache) {
-    entry.textCache = false;
-  }
-}
-
-const applyEvents = (entries: Map<String, Entry>, events: Event[]) => {
-  const t0 = Date.now();
-  let entryCount = 0;
-  let actionCount = 0;
-  events.forEach(event => {
-    const eventId = event.id;
-    if (event.type != 'userAction' || !event.targetIds || !event.targetIds.length || !event.actions || !event.actions.length) {
-      return;
-    }
-    const targetEntries = event.targetIds
-      .map(entryId => entries.get(entryId))
-      .filter(entry => !!entry)
-      .filter(entry => !entry.appliedEventIds || entry.appliedEventIds.indexOf(eventId) < 0);
-
-    targetEntries.forEach(entry => {
-      event.actions.forEach(action => {
-        applyEventAction(entry, action);
-      });
-
-      if (!entry.appliedEventIds) {
-        entry.appliedEventIds = [];
-      }
-      entry.appliedEventIds.push(event.id);
-      actionCount += event.actions.length;
-    })
-    entryCount += targetEntries.length;
-  })
-  console.log(`Applied ${events.length} events to ${entryCount} entries with ${actionCount} actions in ${Date.now() - t0}ms`);
 }
 
 export const eventModel : EventModel = {
@@ -99,7 +28,10 @@ export const eventModel : EventModel = {
   applyEvents: thunk((actions, events, {getStoreState, getStoreActions}) => {
     const storeState = getStoreState();
     const allEntries = storeState.entries.allEntries;
-    applyEvents(allEntries as Map<String, Entry>, events);
+    const t0 = Date.now();
+    const changedEntries = applyEvents(allEntries as Map<String, Entry>, events);
+    changedEntries.forEach((entry: any) => entry.textCache = false)
+    console.log(`Applied ${events.length} events and updated ${changedEntries.length} entries in ${Date.now() - t0}ms`);
     getStoreActions().search.refresh();
   }),
 };
