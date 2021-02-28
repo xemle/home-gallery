f89b940ac58e9e26a016956699f0a260283c07d6
Sebastian Felis <sebastian@silef.de>
2021-02-28 11:04:53 +0100

    Allow gracefully shutown of api server

--- packages/api-server/src/server/index.js
+++ packages/api-server/src/server/index.js
@@ -8,7 +8,19 @@ const server = async ({port}) => {
   app.use(morgan('dev'));
 
   return new Promise(resolve => {
-    app.listen(port, () => {
+    let server;
+
+    const shutDown = () => {
+      console.log(`Stopping server`)
+      server?.close(() => {
+        process.exit(0);
+      })
+    }
+
+    process.on('SIGTERM', shutDown);
+    process.on('SIGINT', shutDown);
+
+    server = app.listen(port, () => {
       console.log(`Listen on port http://localhost:${port}`);
       resolve(app);
     });
