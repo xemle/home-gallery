f2183e621eec8e5a2e6839f14b22934843c454ee
Sebastian Felis <sebastian@silef.de>
2021-02-28 09:37:16 +0100

    Improve docker image build through .dockerignore

--- packages/api-server/download-models.js
+++ packages/api-server/download-models.js
@@ -5,4 +5,5 @@ const download = async () => {
   await downloadModels(modelConfig, modelDir);
 }
 
+console.log(`Downloading required models`);
 download().then(() => console.log(`Download completed`), e => console.log(`Error: ${e}`));
