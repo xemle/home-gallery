d36c1adc502f5076270d1725a252e4f53b37f841
Sebastian Felis <sebastian@silef.de>
2021-03-05 20:47:18 +0100

    Add compressed database export

--- packages/export/src/write-database.js
+++ packages/export/src/write-database.js
@@ -1,18 +1,25 @@
 const path = require('path')
 const debug = require('debug')('export:write-database');
 
-const { writeDatabasePlain } = require('@home-gallery/database');
+const { writeDatabase, writeDatabasePlain } = require('@home-gallery/database');
 
 const write = (database, outputDirectory, basePath, cb) => {
   const t0 = Date.now();
   const filename = path.join(outputDirectory, basePath, 'api', 'database');
+  const gzFilename = `${filename}.gz`;
   writeDatabasePlain(filename, database.data, (err) => {
     if (err) {
       debug(`Could not write database to ${filename}: ${err}`);
       return cb(err);
     }
-    debug(`Wrote database with ${database.data.length} entries to ${filename} in ${Date.now() - t0}ms`)
-    cb(null, database, outputDirectory, basePath);
+    writeDatabase(gzFilename, database.data, (err) => {
+      if (err) {
+        debug(`Could not write compressed database to ${gzFilename}: ${err}`);
+        return cb(err);
+      }
+      debug(`Wrote database with ${database.data.length} entries to ${filename} and ${gzFilename} and in ${Date.now() - t0}ms`)
+      cb(null, database, outputDirectory, basePath);
+    })
   })
 }
 
