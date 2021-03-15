ddd6ef1f96dbefcceeac638cb0704b266edf3728
Sebastian Felis <sebastian@silef.de>
2021-03-15 18:47:24 +0100

    Add recent tag list

--- packages/webapp/src/dialog/tag-dialog.tsx
+++ packages/webapp/src/dialog/tag-dialog.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import { useState } from "react";
 
+import { useStoreState } from '../store/hooks';
+
 export interface TagDialogFormData {
   tags: string;
 }
@@ -11,13 +13,33 @@ export interface TagDialogProps {
   onSubmit: (data: TagDialogFormData) => void;
 }
 
+const Tags = ({tags, addTag}) => {
+  return tags.map((tag, i, a) => (
+    <a key={i} onClick={() => addTag(tag)} className="mr-4" title={`Click to tag '${tag}'`}>{tag}{i < a.length - 1 ? ',' : ''}</a>
+  ))
+}
+
 export const TagDialog = ({onCancel, onSubmit, visible}: TagDialogProps) => {
   const [tags, setTags] = useState("");
+  const recentTags = useStoreState(state => state.events.recentTags);
 
   const submitHandler = (event) => {
     event.preventDefault();
     onSubmit({ tags });
   }
+
+  const addTag = tag => setTags(value => {
+    const match = value.match(new RegExp(`\\b(${tag}\\s*(,\\s*)?)`))
+    if (match) {
+      return value.replace(match[1], '');
+    }
+
+    const strip = value.replace(/\s*,\s*$/, '')
+    if (strip.length) {
+      return `${strip}, ${tag}`
+    }
+    return tag;
+  });
 
   return (
     <div className={`modal ${visible ? '-visible' : ''}`}>
@@ -34,6 +56,10 @@ export const TagDialog = ({onCancel, onSubmit, visible}: TagDialogProps) => {
                 <div className="field">
                   <label htmlFor="tags">Input</label>
                   <input id="tags" className="input" ref={input => input && input.focus()} value={tags} onChange={e => setTags(e.target.value)} />
+                  {recentTags.length &&
+                    <div>
+                      Recent tags: <Tags tags={recentTags.slice(0, 5)} addTag={addTag} />
+                    </div>}
                 </div>
               </div>
             </div>
--- packages/webapp/src/store/event-model.ts
+++ packages/webapp/src/store/event-model.ts
@@ -2,10 +2,30 @@ import { Thunk, thunk } from 'easy-peasy';
 import { Entry } from './entry-model';
 import { StoreModel } from './store';
 
-import { applyEvents, Event } from '@home-gallery/events';
+import { applyEvents, Event, EventAction } from '@home-gallery/events';
+
+const lruAdd = (list: string[], item: string, size: number = 50) => {
+  const index = list.indexOf(item);
+  if (index > 0) {
+    list.splice(index, 1)
+    list.unshift(item);
+  } else if (index < 0 && list.length < size) {
+    list.unshift(item);
+  } else if (index < 0) {
+    list.pop()
+    list.unshift(item)
+  }
+}
+
+const getEventTags = events => events
+  .filter(event => event.type == 'userAction')
+  .map(event => event.actions.filter(a => a.action == 'addTag'))
+  .reduce((all, actions) => all.concat(actions), [])
+  .map(action => action.value);
 
 export interface EventModel {
   events: Event[];
+  recentTags: [];
 
   initEvents: Thunk<EventModel, Event[]>;
   addEvent: Thunk<EventModel, Event>;
@@ -14,6 +34,7 @@ export interface EventModel {
 
 export const eventModel : EventModel = {
   events: [],
+  recentTags: [],
 
   initEvents: thunk(async (actions, payload, {getState}) => {
     const state = getState();
@@ -29,6 +50,7 @@ export const eventModel : EventModel = {
     const storeState = getStoreState();
     const allEntries = storeState.entries.allEntries;
     const t0 = Date.now();
+    getEventTags(events).forEach(tag => lruAdd(storeState.events.recentTags, tag));
     const changedEntries = applyEvents(allEntries as Map<String, Entry>, events);
     changedEntries.forEach((entry: any) => entry.textCache = false)
     console.log(`Applied ${events.length} events and updated ${changedEntries.length} entries in ${Date.now() - t0}ms`);
