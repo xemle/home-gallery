b16d0ab3d3e7b134eec6bece94e1e92ce2679deb
Sebastian Felis <sebastian@silef.de>
2021-07-04 15:35:13 +0200

    Promisify index update

--- packages/index/src/index.js
+++ packages/index/src/index.js
@@ -1,8 +1,7 @@
-const async = require('async');
 const path = require('path');
 const debug = require('debug')('index:update');
 
-const { fileFilter } = require('@home-gallery/common');
+const { fileFilter, promisify, callbackify } = require('@home-gallery/common');
 
 const readIndex = require('./read');
 const createIndex = require('./create');
@@ -15,83 +14,71 @@ const { matcherFns } = require('./merge');
 const { readStream, readStreams } = require('./read-stream');
 const createLimitFilter = require('./limit-filter');
 
-const createOrUpdate = (directory, filename, options, cb) => {
+const asyncReadIndex = promisify(readIndex)
+const asyncFileFilter = promisify(fileFilter)
+const asyncCreateLimitFilter = promisify(createLimitFilter)
+const asyncCreateIndex = promisify(createIndex)
+const asyncUpdateIndex = promisify(updateIndex)
+const asyncWriteIndex = promisify(writeIndex)
+const asyncChecksum = promisify(checksum)
+
+const asyncCreateOrUpdate = async (directory, filename, options) => {
   const now = new Date();
-  async.waterfall([
-    (callback) => readIndex(filename, callback),
-    (fileIndex, callback) => createLimitFilter(fileIndex.data.length, options.addLimits, options.filter, (err, filter) => callback(err, fileIndex, filter)),
-    (fileIndex, limitFilter, callback) => {
-      createIndex(directory, {...options, filter: limitFilter}, (err, fsEntries) => {
-        if (err) {
-          return callback(err);
-        }
-        updateIndex(fileIndex.data, fsEntries, options.matcherFn, (err, entries, changed) => {
-          if (err) {
-            return callback(err);
-          }
-          callback(null, fileIndex, entries, changed, limitFilter.limitExceeded());
-        })
-      })
-    },
-    (fileIndex, entries, changed, limitExceeded, callback) => {
-      if (changed) {
-        const newIndex = {
-          type: 'home-gallery/fileindex@1.0',
-          created: now.toISOString(),
-          base: path.resolve(directory),
-          data: entries
-        }
-        if (options.dryRun) {
-          callback(null, newIndex, limitExceeded);
-        } else {
-          writeIndex(filename, newIndex, (err, index) => callback(err, index, limitExceeded));
-        }
-      } else {
-        callback(null, fileIndex, limitExceeded);
-      }
-    }
-  ], cb);
-}
+  const fileIndex = await asyncReadIndex(filename)
+  const limitFilter = await asyncCreateLimitFilter(fileIndex.data.length, options.addLimits, options.filter)
+  const fsEntries = await asyncCreateIndex(directory, {...options, filter: limitFilter})
+  const [entries, changed ] = await asyncUpdateIndex(fileIndex.data, fsEntries, options.matcherFn)
+  const limitExceeded = limitFilter.limitExceeded()
 
-const updateChecksum = (filename, index, updateChecksums, isDryRun, limitExeeded, cb) => {
-  if (updateChecksums) {
-    const sha1sumDate = new Date().toISOString();
-    return checksum(index, sha1sumDate, (err, index, changed) => {
-      if (err) {
-        return cb(err);
-      } else if (changed && !isDryRun) {
-        return writeIndex(filename, index, (err, index) => cb(err, index, limitExeeded));
-      } else {
-        return cb(null, index, limitExeeded);
-      }
-    })
-  } else {
-    return cb(null, index, limitExeeded);
+  if (!changed) {
+    return [fileIndex, limitExceeded]
   }
+
+  const newIndex = {
+    type: 'home-gallery/fileindex@1.0',
+    created: now.toISOString(),
+    base: path.resolve(directory),
+    data: entries
+  }
+  if (options.dryRun) {
+    return [newIndex, limitExceeded]
+  }
+  const writeIndex = await asyncWriteIndex(filename, newIndex);
+  return [writeIndex, limitExceeded]
 }
 
-const update = (directory, filename, options, cb) => {
+const asyncUpdateChecksum = async (filename, index, updateChecksums, isDryRun) => {
+  if (!updateChecksums) {
+    return index
+  }
+
+  const sha1sumDate = new Date().toISOString();
+  const [updateIndex, changed] = await asyncChecksum(index, sha1sumDate)
+
+  if (!changed || isDryRun) {
+    return index
+  }
+
+  const writeIndex = await asyncWriteIndex(filename, updateIndex);
+  updateIndex = null
+  return writeIndex
+}
+
+const asyncUpdate = async (directory, filename, options) => {
   const t0 = Date.now();
   debug(`Updating file index for directory ${directory}`);
-  async.waterfall([
-    (callback) => fileFilter(options.exclude, options.excludeFromFile, callback),
-    (filter, callback) => createOrUpdate(directory, filename, {...options, filter}, callback),
-    (index, limitExeeded, callback) => updateChecksum(filename, index, options.checksum, options.dryRun, limitExeeded, callback)
-  ], (err, index, limitExeeded) => {
-    if (err) {
-      debug(`Could not update file index ${filename}: ${err}`);
-      cb(err);
-    } else {
-      debug(`Updated file index in ${Date.now() - t0}ms`);
-      cb(null, index, limitExeeded);
-    }
-  });
+
+  const filter = await asyncFileFilter(options.exclude, options.excludeFromFile)
+  const [index, limitExceeded] = await asyncCreateOrUpdate(directory, filename, {...options, filter})
+  const updateIndex = await asyncUpdateChecksum(filename, index, options.checksum, options.dryRun)
+  debug(`Updated file index for directory ${directory} in ${Date.now() - t0}ms`);
+  return [updateIndex, limitExceeded]
 }
 
 module.exports = {
   readStream,
   readStreams,
-  update,
+  update: callbackify(asyncUpdate),
   matcherFns,
   prettyPrint,
   statIndex
