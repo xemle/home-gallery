bc59a16f1194825d053beb905cbc09f2fc094ea9
Sebastian Felis <sebastian@silef.de>
2021-08-02 20:38:08 +0200

    Create dir link latest e2e test suite dir

--- e2e/tests/hooks.js
+++ e2e/tests/hooks.js
@@ -1,3 +1,4 @@
+const fs = require('fs/promises')
 const path = require('path')
 const { rm } = require('shelljs')
 const { generateId, dateFormat } = require('../utils')
@@ -5,11 +6,21 @@ const { generateId, dateFormat } = require('../utils')
 const testBaseDir = process.env.gallery_tmp_dir
 const randomId = !!`${process.env.gallery_tmp_dir_radomize || 'true'}`.toLowerCase().match(/(true|1)/)
 const keepTempDir = !!`${process.env.gallery_tmp_dir_keep || 'true'}`.toLowerCase().match(/(true|1)/)
+const runDirName = 'run-e2e'
 
 beforeSuite(async () => {
   const runId = `${randomId ? `${dateFormat(new Date(), '%Y%M%D-%H%m')}-${generateId(6)}` : 'static'}`
   gauge.dataStore.suiteStore.put('runId', runId)
   gauge.message(`Unique run ID is ${runId}`)
+
+  const currentRunDir = path.join(testBaseDir, runDirName, runId)
+  await fs.mkdir(currentRunDir, { recursive: true } )
+
+  const latest = path.join(testBaseDir, runDirName, 'latest')
+  await fs.access(latest)
+    .then(() => fs.unlink(latest))
+    .finally(() => fs.symlink(currentRunDir, latest))
+    .catch(e => gauge.message(`Could not set latest link to current run dir ${currentRunDir}: ${e}`))
 })
 
 const sanitizeName = name => name.replace(/[^_0-9A-Za-z]/g, '-').replace(/[-]+/g, '-').toLocaleLowerCase()
@@ -18,7 +29,7 @@ const createBaseDir = context => {
   const runId = gauge.dataStore.suiteStore.get('runId')
   const spec = context.currentSpec.name
   const scenario = context.currentScenario.name
-  const baseDir = path.join(testBaseDir, 'run-e2e', runId, sanitizeName(spec), sanitizeName(scenario))
+  const baseDir = path.join(testBaseDir, runDirName, runId, sanitizeName(spec), sanitizeName(scenario))
   gauge.message(`Set test base dir to ${baseDir}`)
   return baseDir
 }
