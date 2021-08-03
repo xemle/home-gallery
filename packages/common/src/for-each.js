2e171a4b32909d87d3ae995dd07294c46d7f062e
Sebastian Felis <sebastian@silef.de>
2021-08-03 12:20:51 +0200

    Add missing file

---
+++ packages/common/src/for-each.js
@@ -0,0 +1,21 @@
+const forEach = (items, onItem, cb) => {
+  let i = 0
+  const result = []
+  
+  const next = () => {
+    if (i === items.length) {
+      return cb(null, result)
+    }
+    onItem(items[i++], (err, ...rest) => {
+      if (err) {
+        return cb(err)
+      }
+      result.push(rest)
+      next()
+    })
+  }
+
+  next()
+}
+
+module.exports = forEach
