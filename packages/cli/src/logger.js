f88cac484a908a52f7e35ab0e8ded75b33f9b455
Sebastian Felis <sebastian@silef.de>
2022-12-02 11:01:17 +0100

    cli: Initialize logger by config

--- packages/cli/src/logger.js
+++ packages/cli/src/logger.js
@@ -1,5 +1,7 @@
 const logger = require('@home-gallery/logger')
 
+const { load } = require('./config')
+
 const loggerOptions = {
   'log-level': {
     alias: 'l',
@@ -20,13 +22,48 @@ const loggerOptions = {
   }
 }
 
+const addFileLogger = async (file, level) => new Promise(resolve => logger.addFile(file, level, resolve))
+
 const loggerMiddleware = async (argv) => {
+  const logMessages = []
+
+  const enabledLoggers = {
+    console: false,
+    file: false
+  }
+
   if (argv.logLevel) {
     logger.addPretty(argv.logLevel)
+    logMessages.push(`Add console logger in level ${argv.logLevel} through cli args`)
+    enabledLoggers.console = true
   }
   if (argv.logFile) {
-    await new Promise(resolve => logger.addFile(argv.logFile, argv.logFileLevel, resolve))
+    await addFileLogger(argv.logFile, argv.logFileLevel)
+    logMessages.push(`Add file logger in level ${argv.logLevel} and file ${argv.logFile} through cli args`)
+    enabledLoggers.file = true
   }
+
+  if (argv.config) {
+    const { config } = load(argv.config).catch(()=> ({}))
+    const loggers = config?.logger || []
+    for (const configLogger of loggers) {
+      if (enabledLoggers[configLogger.type]) {
+        logMessages.push(`Skip ${configLogger.type} logger by config. Logger was already initialized through cli args`)
+        continue
+      }
+
+      if (configLogger.type == 'console') {
+        logger.addPretty(configLogger.level)
+        logMessages.push(`Add console logger in level ${configLogger.level} through cli args`)
+      } else if (configLogger.type == 'file') {
+        await addFileLogger(configLogger.file, configLogger.level)
+        logMessages.push(`Add file logger in level ${configLogger.level} and file ${configLogger.file} through config`)
+      }
+    }
+  }
+
+  const log = logger('cli.logger')
+  logMessages.forEach(msg => log.trace(msg))
 }
 
 module.exports = {
