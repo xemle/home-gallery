aca3ea4a32636287eaea94f47b9d1e7b37530b90
Dukobpa3 <a.burdun@gmail.com>
2024-04-08 14:51:47 +0300

    Edit faces button, rearrange blocks in details view, some polishing

--- packages/webapp/src/single/Details.tsx
+++ packages/webapp/src/single/Details.tsx
@@ -11,18 +11,18 @@ import { Tag } from "../api/models";
 import { useAppConfig } from "../utils/useAppConfig";
 import { classNames } from "../utils/class-names";
 
-export const Details = ({entry, dispatch}) => {
+export const Details = ({ entry, dispatch }) => {
   const appConfig = useAppConfig()
   const navigate = useNavigate();
 
-  const {openDialog, setDialogVisible} = useTagDialog()
+  const { openDialog, setDialogVisible } = useTagDialog()
 
   if (!entry) {
     return (<></>)
   }
 
   const dispatchSearch = (query) => {
-    dispatch({type: 'search', query})
+    dispatch({ type: 'search', query })
   }
 
   const escapeSearchValue = value => /[\s+]/.test(value) ? `"${value}"` : value
@@ -103,19 +103,19 @@ export const Details = ({entry, dispatch}) => {
     }
 
     return (
-      <a key={`${entry.shortId}-geo`} className="text-gray-300 break-all rounded hover:cursor-pointer hover:bg-gray-600 hover:text-gray-200" onClick={() => dispatch({type: 'map'})}>
+      <a key={`${entry.shortId}-geo`} className="text-gray-300 break-all rounded hover:cursor-pointer hover:bg-gray-600 hover:text-gray-200" onClick={() => dispatch({ type: 'map' })}>
         {latitude.toFixed(4)},{longitude.toFixed(4)} (open map)
       </a>
     )
   }
 
-  const origTags: Tag[] = (entry.tags || []).map(name => ({name, remove: false}))
+  const origTags: Tag[] = (entry.tags || []).map(name => ({ name, remove: false }))
 
-  const onSubmit = ({tags}: {tags: Tag[]}) => {
-    const tagNames = tags.map(({name}) => name)
+  const onSubmit = ({ tags }: { tags: Tag[] }) => {
+    const tagNames = tags.map(({ name }) => name)
     const origTagNames = entry.tags || []
-    const tagActions = origTagNames.filter(name => !tagNames.includes(name)).map(name => ({name, remove: true}))
-    tagActions.push(...tagNames.filter(name => !origTagNames.includes(name)).map(name => ({name, remove: false})))
+    const tagActions = origTagNames.filter(name => !tagNames.includes(name)).map(name => ({ name, remove: true }))
+    tagActions.push(...tagNames.filter(name => !origTagNames.includes(name)).map(name => ({ name, remove: false })))
     if (tagActions.length) {
       addTags([entry.id], tagActions).then(() => {
         setDialogVisible(false);
@@ -128,7 +128,7 @@ export const Details = ({entry, dispatch}) => {
   }
 
   const editTags = () => {
-    openDialog({initialTags: origTags, onSubmit})
+    openDialog({ initialTags: origTags, onSubmit })
   }
 
   return (
@@ -137,15 +137,17 @@ export const Details = ({entry, dispatch}) => {
         <div className="flex items-center justify-between mb-4">
           <h3 className="text-xl text-gray-300">Media Details</h3>
           <a className="flex items-center justify-center w-6 h-6 rounded hover:bg-gray-700 hover:cursor-pointer"
-            onClick={() => dispatch({type: 'toggleDetails'})}
+            onClick={() => dispatch({ type: 'toggleDetails' })}
             title="Close media details">
-            <FontAwesomeIcon icon={icons.faXmark} className="text-gray-700 hover:text-gray-400 active:text-gray-200"/>
+            <FontAwesomeIcon icon={icons.faXmark} className="text-gray-700 hover:text-gray-400 active:text-gray-200" />
           </a>
         </div>
         <div className="flex flex-col gap-6 text-gray-500">
+
+          {/* Type */}
           <div className="flex">
             <div className="w-8">
-              <FontAwesomeIcon icon={icons.faIdCard} className="text-gray-300"/>
+              <FontAwesomeIcon icon={icons.faIdCard} className="text-gray-300" />
             </div>
             <div>
               <p>{mainFilename}</p>
@@ -155,84 +157,14 @@ export const Details = ({entry, dispatch}) => {
               )}{entry.width}x{entry.height}</p>
             </div>
           </div>
-          <div className="flex">
-            <div className="w-8">
-              <FontAwesomeIcon icon={icons.faCalendar} className="text-gray-300"/>
-            </div>
-            <div>
-              <p>
-                {searchLink(formatDate('%d', entry.date), `year:${entry.date.substr(0, 4)} month:${entry.date.substr(5, 2)} day:${entry.date.substr(8, 2)}`)}
-                <span className="px-1">.</span>
-                {searchLink(formatDate('%m', entry.date), `year:${entry.date.substr(0, 4)} month:${entry.date.substr(5, 2)}`)}
-                <span className="px-1">.</span>
-                {searchLink(formatDate('%Y', entry.date), `year:${entry.date.substr(0, 4)}`)}
-              </p>
-              <p>
-                {formatDate('%H:%M:%S', entry.date)}
-              </p>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="flex-shrink-0 w-8">
-              <FontAwesomeIcon icon={icons.faFolder} className="text-gray-300"/>
-            </div>
-            <div>
-              {entry.files.map(mapFile).map(l => (
-                <>
-                  <p>{l}</p>
-                </>
-              ))}
-            </div>
-          </div>
-          { (hasAddress(entry) || hasGeo(entry)) && (
-            <div className="flex">
-              <div className="flex-shrink-0 w-8">
-                <FontAwesomeIcon icon={icons.faMapPin} className="text-gray-300"/>
-              </div>
-              <div>
-                {entry.road && (
-                  <p>{simpleSearchLink(entry.road, 'location', entry.road)}</p>
-                )}
-                {entry.city && (
-                  <p>{simpleSearchLink(entry.city, 'city', entry.city)}</p>
-                )}
-                {entry.country && (
-                  <p>{simpleSearchLink(entry.country, 'country', entry.country)}</p>
-                )}
-                {hasGeo(entry) && (
-                  <p className={classNames({'mt-2': hasAddress(entry)})}>{GeoLink(entry)}</p>
-                )}
-              </div>
-            </div>
-          )}
-
-          {/* Tags */}
-          <div className="flex">
-            <div className="flex-shrink-0 w-8">
-              <FontAwesomeIcon icon={icons.faTags} className="text-gray-300"/>
-            </div>
-            <div>
-              <p className="inline-flex flex-wrap gap-2">
-                {entry.tags.map(tag => (
-                  <a className="px-2 py-1 text-gray-300 bg-gray-800 rounded hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer" onClick={() => dispatchSearch(`${queryTerm("tag", tag)}`)} title={`Search for tag: ${tag}`}>{tag}</a>
-                ))}
-                {!appConfig.disabledEdit && (
-                  <a className="flex items-center gap-2 px-2 py-1 text-gray-500 bg-transparent border border-gray-700 rounded group inset-1 hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer active:bg-gray-600" onClick={editTags} title={`Edit single tags`}>
-                    <FontAwesomeIcon icon={icons.faPen} className="text-gray-500 group-hover:text-gray-300"/>
-                    <span>Edit tags</span>
-                  </a>
-                )}
-              </p>
-            </div>
-          </div>
 
           {/* Make */}
           <div className="flex">
             <div className="flex-shrink-0 w-8">
-              <FontAwesomeIcon icon={icons.faCamera} className="text-gray-300"/>
+              <FontAwesomeIcon icon={icons.faCamera} className="text-gray-300" />
             </div>
             <div>
-              { (entry.make || entry.model) && (
+              {(entry.make || entry.model) && (
                 <p>
                   {entry.make && (
                     <>{simpleSearchLink(entry.make, 'make', entry.make)}</>
@@ -249,11 +181,67 @@ export const Details = ({entry, dispatch}) => {
             </div>
           </div>
 
+          {/* Date */}
+          <div className="flex">
+            <div className="w-8">
+              <FontAwesomeIcon icon={icons.faCalendar} className="text-gray-300" />
+            </div>
+            <div>
+              <p>
+                {searchLink(formatDate('%d', entry.date), `year:${entry.date.substr(0, 4)} month:${entry.date.substr(5, 2)} day:${entry.date.substr(8, 2)}`)}
+                <span className="px-1">.</span>
+                {searchLink(formatDate('%m', entry.date), `year:${entry.date.substr(0, 4)} month:${entry.date.substr(5, 2)}`)}
+                <span className="px-1">.</span>
+                {searchLink(formatDate('%Y', entry.date), `year:${entry.date.substr(0, 4)}`)}
+              </p>
+              <p>
+                {formatDate('%H:%M:%S', entry.date)}
+              </p>
+            </div>
+          </div>
+
+          {/* Path */}
+          <div className="flex">
+            <div className="flex-shrink-0 w-8">
+              <FontAwesomeIcon icon={icons.faFolder} className="text-gray-300" />
+            </div>
+            <div>
+              {entry.files.map(mapFile).map(l => (
+                <>
+                  <p>{l}</p>
+                </>
+              ))}
+            </div>
+          </div>
+
+          {/* Geo */}
+          {(hasAddress(entry) || hasGeo(entry)) && (
+            <div className="flex">
+              <div className="flex-shrink-0 w-8">
+                <FontAwesomeIcon icon={icons.faMapPin} className="text-gray-300" />
+              </div>
+              <div>
+                {entry.road && (
+                  <p>{simpleSearchLink(entry.road, 'location', entry.road)}</p>
+                )}
+                {entry.city && (
+                  <p>{simpleSearchLink(entry.city, 'city', entry.city)}</p>
+                )}
+                {entry.country && (
+                  <p>{simpleSearchLink(entry.country, 'country', entry.country)}</p>
+                )}
+                {hasGeo(entry) && (
+                  <p className={classNames({ 'mt-2': hasAddress(entry) })}>{GeoLink(entry)}</p>
+                )}
+              </div>
+            </div>
+          )}
+
           {/* Objects */}
           {entry.objects.length > 0 && (
             <div className="flex">
               <div className="flex-shrink-0 w-8">
-                <FontAwesomeIcon icon={icons.faShapes} className="text-gray-300"/>
+                <FontAwesomeIcon icon={icons.faShapes} className="text-gray-300" />
               </div>
               <div>
                 <p className="inline-flex flex-wrap gap-2">{entry.objects.map(object => (
@@ -263,16 +251,54 @@ export const Details = ({entry, dispatch}) => {
             </div>
           )}
 
+          {/* Tags */}
+          <div className="flex">
+            <div className="flex-shrink-0 w-8">
+              <FontAwesomeIcon icon={icons.faTags} className="text-gray-300" />
+            </div>
+            <div>
+              <p className="inline-flex flex-wrap gap-2">
+
+                {/* {entry.tags.map(tag => (
+                  <span>{simpleSearchLink(tag, 'tag', tag)}, </span>
+                ))} */}
+
+                {entry.tags.map(tag => (
+                  <a className="px-2 py-1 text-gray-300 bg-gray-800 rounded hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer" onClick={() => dispatchSearch(`${queryTerm("tag", tag)}`)} title={`Search for tag: ${tag}`}>{tag}</a>
+                ))}
+                {!appConfig.disabledEdit && (
+                  <a className="flex items-center gap-2 px-2 py-1 text-gray-500 bg-transparent border border-gray-700 rounded group inset-1 hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer active:bg-gray-600" onClick={editTags} title={`Edit single tags`}>
+                    <FontAwesomeIcon icon={icons.faPen} className="text-gray-500 group-hover:text-gray-300" />
+                    {/* <span>Edit tags</span> */}
+                  </a>
+                )}
+              </p>
+            </div>
+          </div>
+
           {/* Faces */}
           {entry.faces.length > 0 && (
             <div className="flex">
               <div className="flex-shrink-0 w-8">
-                <FontAwesomeIcon icon={icons.faUser} className="text-gray-300"/>
+                <FontAwesomeIcon icon={icons.faUser} className="text-gray-300" />
               </div>
               <div>
-                <p className="inline-flex flex-wrap gap-2">{entry.faces.map((face, i) => (
+                <p className="inline-flex flex-wrap gap-2">
+                  {/* {entry.faces.map((face, i) => (
                   <span>{selectFace(face.gender, entry.shortId, i)} (~{face.age.toFixed()}y)</span>
-                ))}</p>
+                ))} */}
+
+                  {entry.faces.map((face, i) => (
+                    <a className="px-2 py-1 text-gray-300 bg-gray-800 rounded hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer" onClick={() => navigate(`/faces/${entry.shortId}/${i}`)} title={`Search for person: ${face.gender} (${face.age.toFixed()}y)`}>{face.gender} ({~face.age.toFixed()}y)</a>
+                  ))}
+
+                  {!appConfig.disabledEdit && (
+                    <a className="flex items-center gap-2 px-2 py-1 text-gray-500 bg-transparent border border-gray-700 rounded group inset-1 hover:bg-gray-700 hover:text-gray-200 hover:cursor-pointer active:bg-gray-600" onClick={editTags} title={`Edit single tags`}>
+                      <FontAwesomeIcon icon={icons.faPen} className="text-gray-500 group-hover:text-gray-300" />
+                      {/* <span>Edit tags</span> */}
+                    </a>
+                  )}
+                </p>
               </div>
             </div>
           )}
