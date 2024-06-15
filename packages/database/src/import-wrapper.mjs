477f1df5ad7ea1580b0f603dd9a556e534fec82e
Sebastian Felis <sebastian@silef.de>
2024-06-15 14:45:52 +0200

    database: Add ESM wrapper

---
+++ packages/database/src/import-wrapper.mjs
@@ -0,0 +1,11 @@
+import index from './index.js'
+
+export const buildDatabase = index.buildDatabase
+export const HeaderType = index.HeaderType
+export const isDatabaseTypeCompatible = index.isDatabaseTypeCompatible
+export const mergeEntries = index.mergeEntries
+export const readDatabase = index.readDatabase
+export const readOrCreateDatabase = index.readOrCreateDatabase
+export const migrate = index.migrate
+export const writeDatabase = index.writeDatabase
+export const writeDatabasePlain = index.writeDatabasePlain
