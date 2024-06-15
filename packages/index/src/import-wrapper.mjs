2c1dae42aee32bc43cfd30703211b35ee3cac01c
Sebastian Felis <sebastian@silef.de>
2024-06-15 14:30:32 +0200

    index: Add ESM wrapper

---
+++ packages/index/src/import-wrapper.mjs
@@ -0,0 +1,13 @@
+import index from './index.js'
+
+export const getIndexName = index.getIndexName
+export const getJournalFilename = index.getJournalFilename
+export const removeJournal = index.removeJournal
+export const readIndexHead = index.readIndexHead
+export const readStream = index.readStream
+export const readStreams = index.readStreams
+export const readJournal = index.readJournal
+export const update = index.update
+export const matcherFns = index.matcherFns
+export const prettyPrint = index.prettyPrint
+export const statIndex = index.statIndex
