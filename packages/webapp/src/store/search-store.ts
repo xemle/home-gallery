c036b9cd644d94125399fb03f9446d8e11fcc447
Sebastian Felis <sebastian@silef.de>
2024-08-21 08:20:31 +0200

    webapp: Remove old search code

--- packages/webapp/src/store/search-store.ts
+++ packages/webapp/src/store/search-store.ts
@@ -1,14 +1,6 @@
 import { create } from 'zustand'
 import { persist } from 'zustand/middleware'
 
-import { Entry } from './entry'
-import { useEntryStore } from './entry-store'
-
-import { execQuery } from './query/query'
-import { execSimilar } from './query/similar'
-import { execFaces } from './query/faces'
-import { byDateDesc } from './query/utils'
-
 export interface Search {
   type: 'none' | 'year' | 'query' | 'similar' | 'faces'
   value?: any
@@ -18,33 +10,6 @@ export interface Search {
 export interface SearchStore {
   query: Search
   search: (query: Search) => void
-}
-
-const doSearch = async (entries: Entry[], query) => {
-  if (!entries.length) {
-    return entries;
-  }
-  entries.sort(byDateDesc)
-
-  if (query.type == 'query') {
-    entries = await execQuery(entries, query.value)
-  } else if (query.type == 'year') {
-    entries = await execQuery(entries, `year:${query.value} order by date asc`)
-  } else if (query.type == 'similar') {
-    const id = query.value
-    const seedEntry = entries.find(entry => entry.id.startsWith(id))
-    entries = execSimilar(entries, seedEntry?.similarityHash)
-  } else if (query.type == 'faces') {
-    const { id, faceIndex } = query.value
-    const seedEntry = entries.find(entry => entry.id.startsWith(id))
-    const descriptor = seedEntry?.faces[faceIndex]?.descriptor
-    entries = execFaces(entries, descriptor)
-  }
-  if (query.query) {
-    entries = await execQuery(entries, query.query)
-  }
-
-  return entries
 }
 
 export const useSearchStore = create<
