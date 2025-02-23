221bd29d21c8b5a1ae532f1633a998b86e7bcc80
Sebastian Felis <sebastian@silef.de>
2025-02-23 10:52:39 +0100

    extractor: Recreate ffprobe if required

--- packages/extractor/src/extract/meta/ffprobe.js
+++ packages/extractor/src/extract/meta/ffprobe.js
@@ -12,13 +12,22 @@ const ffprobeSuffix = 'ffprobe.json';
  * @returns {import('stream').Transform}
  */
 export function ffprobe(storage, ffprobePath) {
-  const test = entry => entry.type === 'video' && !storage.hasFile(entry, ffprobeSuffix);
+  const test = entry => entry.type === 'video' && needsUpgrade(entry.meta?.ffprobe)
+
+  const _gallery = { version: 2 }
 
   const task = async (entry) => {
     const t0 = Date.now();
     const src = await entry.getFile()
-    return callFfprobe(ffprobePath, src)
-      .then(data => storage.writeFile(entry, ffprobeSuffix, data))
+    return spawnFfprobe(ffprobePath, src)
+      .then(stdout => {
+        try {
+          return JSON.parse(stdout)
+        } catch (e) {
+          throw new Error(`Failed to parse out as json: ${stdout}`, {cause: e})
+        }
+      })
+      .then(data => storage.writeFile(entry, ffprobeSuffix, {...data, _gallery}))
       .then(() => {
         log.debug(t0, `Extracted video meta data from ${entry}`);
       })
@@ -33,31 +42,38 @@ export function ffprobe(storage, ffprobePath) {
   }
 }
 
-async function callFfprobe(ffprobePath, src) {
+async function spawnFfprobe(ffprobePath, src) {
   const args = '-v quiet -print_format json -show_format -show_streams'.split(' ')
   args.push(src)
   return new Promise((resolve, reject) => {
-    const stdout = []
-    const stderr = []
+    const stdoutData = []
+    const stderrData = []
+    let errorHandled = false
 
     const ffprobe = spawn(ffprobePath, args, { stdio: 'pipe' })
-    ffprobe.once('exit', code => {
-      if (code) {
-        const lastLine = stderr.join('').split('\n').filter(Boolean).pop()
-        reject(new Error(`Failed to run ffprobe. Exit code is ${code}: ${lastLine}`))
+    ffprobe.once('exit', (code, signal) => {
+      if (errorHandled) {
         return
       }
-      const out = stdout.join('')
-      try {
-        const json = JSON.parse(out)
-        return resolve(json)
-      } catch (e) {
-        reject(new Error(`Failed to parse out as json: ${out}`, {cause: e}))
+      const stdout = stdoutData.join('')
+      const stderr = stderrData.join('')
+      const lastErrLine = stderr.split('\n').filter(Boolean).pop()
+      if (signal) {
+        return reject(new Error(`Failed to run ffprobe. Terminated with signal ${signal}: ${lastErrLine}`))
       }
+      if (code) {
+        return reject(new Error(`Failed to run ffprobe. Exit code is ${code}: ${lastErrLine}`))
+      }
+
+      resolve(stdout)
+    })
+    ffprobe.once('error', cause => {
+      errorHandled = true
+      reject(new Error(`Failed to run ffprobe: ${cause}`, { cause }))
     })
 
-    ffprobe.stdout.on('data', data => stdout.push(data.toString()))
-    ffprobe.stderr.on('data', data => stderr.push(data.toString()))
+    ffprobe.stdout.on('data', data => stdoutData.push(data.toString()))
+    ffprobe.stderr.on('data', data => stderrData.push(data.toString()))
   })
 }
 
@@ -83,3 +99,16 @@ export const ffprobePlugin = manager => ({
     return ffprobe(storage, ffprobePath)
   },
 })
+
+function needsUpgrade(ffprobe) {
+  if (!ffprobe) {
+    return true
+  }
+
+  const version = ffprobe._gallery?.version || 1
+  if (version == 1) {
+    log.debug(`Recreate ffprobe due extractor upgrade`)
+    return true
+  }
+  return false
+}
