3e2b35c884cb36490d710db7c936b751d3b2db59
Sebastian Felis <sebastian@silef.de>
2025-08-15 22:05:53 +0200

    webapp: Fix rerenders

--- packages/webapp/src/list/Similar.tsx
+++ packages/webapp/src/list/Similar.tsx
@@ -7,13 +7,17 @@ import {
 
 import { List } from './List';
 import { useSearchStore } from '../store/search-store'
+import { useEffect } from "react";
 
 export const SimilarView = () => {
   const params = useParams();
   const location = useLocation();
   const search = useSearchStore(state => state.search);
-  let locationQuery = new URLSearchParams(location.search && location.search.substring(1) || '');
-  search({type: 'similar', value: params.id, query: locationQuery.get('q') || ''});
+
+  useEffect(() => {
+    let locationQuery = new URLSearchParams(location.search && location.search.substring(1) || '');
+    search({type: 'similar', value: params.id, query: locationQuery.get('q') || ''});
+  }, [params.id, location.search])
 
   return ( 
     <List />
