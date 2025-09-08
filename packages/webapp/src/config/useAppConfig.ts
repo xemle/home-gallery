944e95202b5846f6d6235d703be28938379870b4
Bugz000 <Bugzy000@hotmail.co.uk>
2025-09-08 06:29:17 +0100

    Added support for configurable title
    webapp:
      siteTitle: "My Gallery"

--- packages/webapp/src/config/useAppConfig.ts
+++ packages/webapp/src/config/useAppConfig.ts
@@ -2,6 +2,7 @@ import { useMemo } from "react";
 import { type AppConfig } from "./AppConfig";
 
 const defaultConfig: AppConfig = {
+  siteTitle: "Home Gallery",
   HQzoom: false,
   disabled: [],
   pluginManager: {
