cc41069d0fcc5c82e4a200d19cbf26b53a6361f0
Sebastian Felis <sebastian@silef.de>
2025-09-28 22:33:12 +0200

    webapp: Cleanup appConfig

--- packages/webapp/src/config/useAppConfig.ts
+++ packages/webapp/src/config/useAppConfig.ts
@@ -2,15 +2,6 @@ import { useMemo } from "react";
 import { type AppConfig } from "./AppConfig";
 
 const defaultConfig: AppConfig = {
-  /**
-   * List of disabled features
-   *
-   * - edit: No edit menu button
-   * - serverEvents: No subscription to server sent events (SSE)
-   * - offlineDatabase: No offline database via IndexedDB
-   *
-   * A feature will be expanded to `disabledEdit: true`
-   */
   disabled: [],
   pluginManager: {
     plugins: []
@@ -35,11 +26,6 @@ export const useAppConfig = () => {
 
     const searchParams = new URLSearchParams(location.search?.substring(1) || '')
     result.disabled.push(...searchParams.getAll('disabled').filter(v => !!v))
-
-    result.disabled.forEach((feature: string) => {
-      const name = `disabled${feature[0].toUpperCase()}${feature.slice(1)}`
-      result[name] = true
-    })
 
     return result as AppConfig
   }, [])
